9baee68f19ed3e80329324299b968a6e9ecbe9ba
s2ahil <sahilpradhan411@gmail.com>
2024-09-26 13:09:11 +0530

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+
+    <div class="" style="display: flex; flex-direction: column; align-items: center;">
+      <img src="/mern.png" alt="Discover Nuxt 3" />
+      <div>mern </div>
+    </div>
+  </div>
+</template>
+<!-- About Me
+- Skills
+- Projects
+- Contact Info -->
